9ecb18703a02738d3ef35c4ea79492c543e3c4e0
오성애 <>
2022-05-24 13:35:03 +0900

    자바스크립트
    니꼬 선생님 강의 듣는 중

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const title = document.getElementById("title");
+//const loginForm = document.getElementById("login-form");
+//const loginInput = loginForm.querySelector("input");
+//const loginButton= loginForm.querySelector("button");
 
-title.innerText = "Got you!";
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-console.log(title.id);
-console.log(title.className);
+const HIDDEN_CLASSNAME = "hidden"; //string 문자를 담는거라서, 관습 #css 클네임에 hidden이 있어서 따로 선언
+
+function onLoginSubmit(event) {
+  event.preventDefault(); //기본동작이 실행되지 않게, 새로고침
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
